8291e23f57302510f905dd7a73e292f8e1025fa3
Julia <julia.mateus@fatec.sp.gov.br>
2024-08-16 20:10:31 -0300

    first commit

---
+++ import os.py
@@ -0,0 +1,24 @@
+import os
+os.system('cls')
+
+nome = "João da Silva"
+
+#for letra in nome:
+ #   print(letra)
+
+for i in range(5):
+    print(i)
+
+for i in range(1,10,2):
+    print(i)
+
+for i in range(10,1,-1):
+    print(i)
+
+soma = 0
+for i in range(5):
+    soma +-i
+else:
+    print("Soma = %d" %soma)
+
+    
