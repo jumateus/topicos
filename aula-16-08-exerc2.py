f0a1ed81a6081c2c3e6e9594a18cc0b4a4114640
Julia <julia.mateus@fatec.sp.gov.br>
2024-08-16 21:48:29 -0300

    rrrr

--- aula-16-08-exerc2.py
+++ aula-16-08-exerc2.py
@@ -1,2 +1,16 @@
 import os
 os.system('cls')
+
+r1 = float(input("Digite o valor da resistência 1: "))
+r2 = float(input("Digite o valor da resistência 2: "))
+r3 = float(input("Digite o valor da resistência 3: "))
+r4 = float(input("Digite o valor da resistência 4: "))
+
+maior = 5000000000000
+menor = -999999999999
+
+if r1 > maior:
+    maior = r1
+    elif
+
+print("Resistências fornecidas: {r1}, {r2}, {r3}, {r4}")
